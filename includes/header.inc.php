3443d6ababf00afa98a009eca39fd35cdfca1612
wuqianye <natsumi3613@gmail.com>
2020-05-06 16:39:38 -0400

    modified 5/6 16:39

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,5 +1,15 @@
-<header class="jumbotron">
+<header class="container-fluid">
 	<nav class="navbar navbar-dark bg-dark">
         <span class="navbar-brand">Placeholder</span>
     </nav>
+    <div class="jumbotron">
+        <form action="#" method="POST">
+            <div class="input-group input-group-lg">
+                <input class="form-control" type="text" name="search" placeholder="Search for Product">
+                <div class="input-group-append">
+                    <button type="button" class="btn btn-dark"><i class="material-icons">search</i></button>
+                </div>
+            </div>
+        </form>
+    </div>
 </header>
