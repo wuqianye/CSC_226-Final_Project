dacf2fe96fa25f1562006da122e9ef48d679e156
wuqianye <natsumi3613@gmail.com>
2020-05-06 23:37:03 -0400

    signup completed

---
+++ signup.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Signup</title>
+		<!-- css -->
+		<link rel="stylesheet" href="assets/signup.css" type="text/css">
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="container flex-container">
+				<h1 class="text-dark mt-3 mb-3">Welcome!</h1>
+				<form action="includes/signup.inc.php" method = "POST">
+					<!--username-->
+					<div class="input-group input-group-lg">
+						<span class="input-group-prepend input-group-text text-white bg-dark">Username</span>
+						<input class="form-control" type="text" name="username" required>
+					</div>
+					<!--password-->
+					<div class="input-group input-group-lg">
+						<span class="input-group-prepend input-group-text text-white bg-dark">Password</span>
+						<input class="form-control" type="password" name="password" required>
+					</div>
+					<!--signup-->
+					<button id="signup" class="btn btn-dark btn-lg" type="submit" name="signup" value="signup">Signup</button>					
+				</form>
+				<!-- login -->
+				<p class="text-dark mb-0">Already have an accout?</p>
+				<!-- jump to login.php -->
+				<button class="btn btn-outline-dark"><a href="login.php" class="text-dark">Login</a></button>
+			</div>
+		</div>
+	</body>
+</html>
